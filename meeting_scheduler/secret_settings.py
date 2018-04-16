a6f8339fe25cd1e2ff165c4819290b866a3ff2f6
Nalintha <slnali@hotmail.co.uk>
2018-04-16 05:54:48 +0100

    uploading secret details for use in marking
    uploading secret details for use in marking

---
+++ meeting_scheduler/secret_settings.py
@@ -0,0 +1,9 @@
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'imeetingbooker@gmail.com'
+
+#outlook api secret key
+CLIENT_SECRET = ''
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+GOOGLE_RECAPTCHA_SECRET_KEY = ''
